80d24a7b82fd20747bc024609104450ff04a8abd
daydayhappychao <573746388@qq.com>
2024-01-23 18:27:58 +0800

    fix: it's run !

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ use std::{
     mpsc::{channel, Receiver, Sender},
     Arc, Mutex,
   },
-  thread,
+  thread::{self, JoinHandle},
   time::Duration,
 };
 
@@ -17,6 +17,7 @@ use napi_derive::napi;
 use option::{Options, Spinner};
 use strip_ansi_escapes::strip_str;
 use termion::terminal_size;
+use utils::stream::{self, Stream};
 
 type ChannelData = (Option<i32>, Option<Vec<&'static str>>);
 
@@ -25,12 +26,15 @@ pub struct Ora {
   options: Options,
   pub line_count: u32,
   pub indent: u32,
-  pub text: String,
   pub is_slient: bool,
   pub is_enable: bool,
-  pub is_spinning: bool,
   pub is_tty: bool,
+  text: Arc<Mutex<String>>,
+  is_spinning: Arc<Mutex<bool>>,
   spinner: Arc<Mutex<option::Spinner>>,
+  join: Option<JoinHandle<()>>,
+  stream: Arc<Mutex<Stream>>,
+  frame_index: Arc<Mutex<u32>>,
 }
 
 #[napi]
@@ -47,17 +51,22 @@ impl Ora {
       options: inner_options.clone(),
       line_count: 0,
       indent: inner_options.indent.unwrap_or(0),
-      text: inner_options.text.unwrap_or("".to_string()),
+      text: Arc::new(Mutex::new(
+        inner_options.text.unwrap_or("".to_string()).to_string(),
+      )),
       is_slient: inner_options.is_silent.unwrap_or(false),
       is_enable: inner_options.is_enable.unwrap_or(true),
-      is_spinning: false,
+      is_spinning: Arc::new(Mutex::new(false)),
       is_tty: atty::is(atty::Stream::Stdout),
       spinner: Arc::new(Mutex::new(inner_options.spinner.unwrap_or(Spinner {
         interval: 80,
         frames: vec!["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"],
       }))),
+      join: None,
+      stream: Arc::new(Mutex::new(Stream::default())),
+      frame_index: Arc::new(Mutex::new(0)),
     };
-    ora.update_line_count();
+    // ora.update_line_count();
 
     ora
   }
@@ -69,11 +78,14 @@ impl Ora {
     let full_prefix_text = self.get_full_prefix_text(&self.options.prefix_text, "-");
     let full_suffix_text = self.get_full_suffix_test(&self.options.suffix_text, " ");
 
+    let text_arc = Arc::clone(&self.text);
+    let text = text_arc.lock().unwrap();
+
     let full_text = format!(
       "{}{}--{}--{}",
       " ".repeat(self.indent.try_into().unwrap()),
       full_prefix_text,
-      self.text,
+      text,
       full_suffix_text
     );
     let mut next_line_count = 0;
@@ -109,16 +121,54 @@ impl Ora {
   }
 
   #[napi]
-  pub fn start(&mut self, text: Option<&str>) -> &mut Ora {
-    self.text = text.unwrap_or(&self.text).to_string();
-    self.is_spinning = true;
-
+  pub fn start(&mut self, text: Option<&str>) -> Ora {
+    let self_text = Arc::clone(&self.text);
     let spinner = Arc::clone(&self.spinner);
-    thread::spawn(move || loop {
-      let mut spinner = spinner.lock().unwrap();
-      thread::sleep(Duration::from_millis(spinner.interval as u64));
+    let stream = Arc::clone(&self.stream);
+    let is_spinning = Arc::clone(&self.is_spinning);
+    let frame_index = Arc::clone(&self.frame_index);
+
+    {
+      let mut inner_is_spinning = is_spinning.lock().unwrap();
+      *inner_is_spinning = true;
+      let mut text_value = self_text.lock().unwrap();
+      *text_value = text.unwrap_or((*text_value).as_str()).to_string();
+    }
+
+    let join = thread::spawn(move || 'outer: loop {
+      let inner_spinner = spinner.lock().unwrap();
+      let inner_stream = stream.lock().unwrap();
+      let inner_is_spinning = is_spinning.lock().unwrap();
+      let mut inner_frame_index = frame_index.lock().unwrap();
+      let inner_text = self_text.lock().unwrap();
+
+      if *inner_is_spinning == false {
+        break 'outer;
+      }
+
+      let interval = inner_spinner.interval.clone();
+      let frames = inner_spinner.frames.clone();
+
+      if *inner_frame_index > frames.len() as u32 - 1 {
+        *inner_frame_index = 0;
+      }
+
+      inner_stream
+        .write(
+          inner_spinner
+            .frames
+            .get(*inner_frame_index as usize)
+            .unwrap(),
+          (*inner_text).as_str(),
+          None,
+          None,
+        )
+        .expect("Write Failed");
+      *inner_frame_index += 1;
+      thread::sleep(Duration::from_millis(interval as u64));
     });
 
+    self.join = Some(join);
     self
   }
 
--- test.js
+++ test.js
@@ -4,7 +4,19 @@ const myOra = require("./index");
 // const spinner = ora("Loading unicorns").start();
 // spinner.text = "loading";
 
-const o = new myOra.Ora({ text: "s" });
-
+const o = new myOra.Ora({
+  text: "s",
+  prefixText: "你",
+  prefixTextFn: "",
+  suffixText: "我",
+  color: "cyan",
+  hideCursour: false,
+  indent: 0,
+  interval: 100,
+  isEnable: true,
+  isSilent: false,
+  discardStdin: false,
+});
+o.start("haluomotuo");
 while (true) {}
 // console.log(124);
